352dc7016e37987c947126c4bb5a3a243305a6a0
Dodex <dodexpwnz@gmail.com>
2022-09-05 22:16:50 +0300

    update 05.09

--- py_basic/file-processing/Exercises/word_search.py
+++ py_basic/file-processing/Exercises/word_search.py
@@ -1,18 +1,18 @@
+import os
+
 def search(word, text):
-    """Return tuple holding line num and line text (or None)."""
-    pass
+    result = []
+    [result.append(lines) for lines in enumerate(text, 1) if word in lines[1]]
+    return result 
 
 def main():
-    # Open my_files/zen_of_python.txt and
-    # create a list from its contents
-    # Save the list as zop
-
+    os.chdir(os.path.dirname(__file__))
+    with open("my_files/zen_of_python.txt") as f:
+        zop = f.read().splitlines()[4:]
+    
     word = input('Enter search word: ')
     result = search(word, zop)
-    if result:
-        print(word, ' first appears on line ',
-              result[0], ': ', result[1], sep='')
-    else:
-        print(word, 'was not found.')
+    [print(word, ' appears on line ', i[0], ': ', i[1], sep='') for i in result]\
+         if result else print(word, 'was not found.')
 
 main()
