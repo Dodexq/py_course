49c84721b4cdf655f353b1ea58160c306caf9df7
Dodex <dodexpwnz@gmail.com>
2022-09-12 19:07:53 +0300

    update 12.09

--- py_advanced/advanced-python-concepts/Exercises/counter.py
+++ py_advanced/advanced-python-concepts/Exercises/counter.py
@@ -1,9 +1,12 @@
 from collections import Counter
+import os
+
+os.chdir(os.path.dirname(__file__))
 
 with open('Declaration_of_Independence.txt') as f:
-    doi = f.read()
+    list = f.read()
 
-word_list = [word for word in doi.upper().split() if len(word) > 5]
+word_list = [word for word in list.upper().split() if len(word) > 5]
     
 c = Counter(word_list)
 print(c.most_common(10))
