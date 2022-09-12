801a4fd891ea10c43cdfb3816d19c2910e5a7d50
Dodex <dodexpwnz@gmail.com>
2022-09-12 22:46:38 +0300

    update 12.09

--- py_advanced/advanced-python-concepts/Exercises/converting_date_string_to_datetime.py
+++ py_advanced/advanced-python-concepts/Exercises/converting_date_string_to_datetime.py
@@ -6,8 +6,11 @@ def str_to_date(str_date):
 
 def str_to_date_map(str_date):
     date_parts = map(int, str_date.split("-"))
-    print(date_parts)
+    return datetime.date(*date_parts)
 
 str_date = input('Input date as YYYY-MM-DD: ')
 date = str_to_date(str_date)
+date2 = str_to_date_map(str_date)
+
 print(date)
+print(date2)
