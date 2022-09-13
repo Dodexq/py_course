0af5c830378cdcb326c372232392cf6fd23fd4a3
Dodex <dodexpwnz@gmail.com>
2022-09-13 23:21:23 +0300

    update 13.09

--- py_advanced/regular-expressions/Exercises/grades.py
+++ py_advanced/regular-expressions/Exercises/grades.py
@@ -3,7 +3,7 @@ import os
 
 os.chdir(os.path.dirname(__file__))
 
-def main():
+def re_compile():
     pattern = re.compile("subj: (.*) grades: (.*)$", re.MULTILINE)
     with open("grades.txt", "r", encoding="utf8") as f:
         for i in f:
@@ -11,5 +11,15 @@ def main():
             if not match_obj:
                 pass
             print(match_obj.groups(0))
+
+def re_finditer():
+    pattern = re.compile("subj: (.*) grades: (.*)$", re.MULTILINE)
+    with open("grades.txt", "r", encoding="utf8") as f:
+        mo_iter = pattern.finditer(f.read())
+        print(dict([mo.groups() for mo in mo_iter]))
+
+def main():
+    re_compile()
+    re_finditer()
 
 main()
