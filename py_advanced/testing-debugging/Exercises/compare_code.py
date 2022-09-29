b73d1d4feb758cff204bcb0fa944351e30299496
Dodex <dodexpwnz@gmail.com>
2022-09-29 22:10:05 +0300

    update 29.09

--- py_advanced/testing-debugging/Exercises/compare_code.py
+++ py_advanced/testing-debugging/Exercises/compare_code.py
@@ -1,5 +1,6 @@
 import re
 import random
+from timeit import repeat
 
 def get_word():
     words = ['Charlie', 'Woodstock', 'Snoopy', 'Lucy', 'Linus',
@@ -20,3 +21,9 @@ def green_glass_door_2():
     word = get_word()
     pattern = re.compile(r'(.)\1')
     return pattern.search(word)
+
+tit1 = repeat(green_glass_door_1, number=1000, repeat=4, globals=globals())
+tit2 = repeat(green_glass_door_2, number=1000, repeat=4, globals=globals())
+
+print(tit1, tit2, sep="\n")
+print(sum(tit1)/sum(tit2))
---
+++ py_advanced/testing-debugging/Exercises/test_intro.py
@@ -0,0 +1,4 @@
+import unittest
+
+class TestMyMethods(unittest.TestCase):
+    pass
